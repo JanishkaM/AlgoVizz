76831a764495b5c5e5dd8de52730c9ef50a4cb09
JanishkaM <janishkawebdesign@gmail.com>
2026-05-11 22:06:21 +0530

    Add UI controls and sorting visualization logic
    Added ComboBoxes, Buttons, Labels, and TextBoxes to Form1 for user interaction. Implemented logic for generating and visualizing arrays, selecting sorting algorithms (Insertion Sort, Merge Sort), and adjusting visualization speed. Enhanced user input handling and error messages.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            comboBox3 = new ComboBox();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(590, 397);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint_1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(9, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "10", "20", "30", "40" });
+            comboBox1.Location = new Point(608, 31);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(180, 23);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Insertion Sort", "Merge Sort" });
+            comboBox2.Location = new Point(608, 87);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(180, 23);
+            comboBox2.TabIndex = 3;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(608, 197);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(180, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged_2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(90, 415);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Slow", "Normal", "Fast" });
+            comboBox3.Location = new Point(608, 146);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(180, 23);
+            comboBox3.TabIndex = 6;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged_1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(171, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 7;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(608, 226);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(180, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(608, 255);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(180, 23);
+            textBox3.TabIndex = 9;
+            textBox3.TextChanged += textBox3_TextChanged;
+            textBox3.Enter += textBox3_Enter_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(608, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 10;
+            label1.Text = "label1";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(608, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 11;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(608, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 12;
+            label3.Text = "label3";
+            label3.Click += label3_Click_1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(button3);
+            Controls.Add(comboBox3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "AlgoVizz";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private Button button2;
+        private ComboBox comboBox3;
+        private Button button3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,5 @@
-using AlgoVizz.Entity;
 using AlgoVizz.Algorithms;
+using AlgoVizz.Entity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AlgoVizz
 {
@@ -17,6 +18,207 @@ namespace AlgoVizz
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private Graphics graphics;
+        private List<Element> elements = new List<Element>();
+        int tk = 20;
+        int ICount;
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                elements.Clear();
+                panel1.Refresh();
+
+                string[] myText = textBox3.Text.Split(' ');
+                int[] arr = new int[myText.Length];
+
+                int height = panel1.Height;
+                int padding = 30 + 5;
+
+                for (int i = 0; i < myText.Length; i++)
+                {
+                    arr[i] = int.Parse(myText[i]);
+                }
+
+                int size = arr.Length;
+                int length = panel1.Bottom;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var start = new Point(padding, height);
+                    int value = height - arr[i] * 10;
+                    var end = new Point(padding, value);
+                    int rvalue = height - value;
+                    var element = new Element(start, end, rvalue);
+                    elements.Add(element);
+                    padding += 40;
+                    element.Draw(graphics, tk);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Enter Elements Seperated by Space", "Enter Elements Seperated by Space", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            String speed = comboBox3.Text;
+            int s;
+            if (speed == "Slow")
+            {
+                s = 700;
+            }
+            else if (speed == "Normal")
+            {
+                s = 200;
+            }
+            else if (speed == "Fast")
+            {
+                s = 50;
+            }
+            else
+            {
+                s = 200;
+            }
+            try
+            {
+                String algo = comboBox2.Text;
+                if (algo == "") //If No Sorting algorithm is selected 
+                {
+                    throw new Exception();
+                }
+                else if (algo == "Insertion Sort")
+                {
+                    ICount = Algorithms.InsertionSort.Sort(elements, graphics, tk, s);
+                    textBox1.Text = String.Format("This is an in-place sorting algorithm based on the idea that one element from the input elements is consumed in each iteration to find its correct position i.e, the position to which it belongs in a sorted array." +
+                    "\r\n\r\nTime Complexity: O(N^2)" + "\r\n\r\nTotal No. of Iterations: {0}", ICount);
+                }
+                else if (algo == "Merge Sort")
+                {
+                    ICount = Algorithms.MergeSort.Sort(elements, graphics, tk, s);
+                    textBox1.Text = String.Format("Merge Sort is a divide-and-conquer algorithm based on the idea of breaking down a list into several sub-lists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list." +
+                    "\r\n\r\nTime Complexity: O(N log(N)) " + "\r\n\r\nTotal No. of Iterations: {0}", ICount);
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please Select Sorting Algorithm", "Select Sorting Algorithm", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Bubble Sort")
+            {
+                textBox1.Text = "Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if the are in wrong order." +
+                    "\r\n\r\nTime Complexity: O(N^2)";
+
+            }
+            else if (comboBox2.Text == "Selection Sort")
+            {
+                textBox1.Text = "Selection Sort algorithm sorts an array by repeatedly finding the minimum element and from the unsorted part and putting it at the beginning." +
+                    "\r\n\r\nTime Complexity: O(N^2)";
+            }
+            else if (comboBox2.Text == "Quick Sort")
+            {
+                textBox1.Text = "Quicksort is a divide-and-conquer algorithm. It works by selecting the a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot." +
+                    "\r\n\r\nTime Complexity: O(N^2)";
+            }
+            else if (comboBox2.Text == "Merge Sort")
+            {
+                textBox1.Text = "Merge Sort is a divide-and-conquer algorithm based on the idea of breaking down a list into several sub-lists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list." +
+                    "\r\n\r\nTime Complexity: O(N log(N))";
+            }
+            else if (comboBox2.Text == "Insertion Sort")
+            {
+                textBox1.Text = "This is an in-place sorting algorithm based on the idea that one element from the input elements is consumed in each iteration to find its correct position i.e, the position to which it belongs in a sorted array." +
+                    "\r\n\r\nTime Complexity: O(N^2)";
+            }
+            else
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Enter_1(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Enter Values Seperated by space")
+            {
+                textBox3.Text = "";
+                textBox3.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                elements.Clear();
+                panel1.Refresh();
+                var rand = new Random();
+                int height = panel1.Height;
+                int padding = 30 + 5;
+
+
+                int size = Convert.ToInt32(comboBox1.Text);
+                int length = panel1.Bottom;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var start = new Point(padding, height);
+                    int value = height - rand.Next(1, 30) * 10;
+                    var end = new Point(padding, value);
+                    int rvalue = height - value;
+                    var element = new Element(start, end, rvalue);
+                    elements.Add(element);
+                    padding += 40;
+                    element.Draw(graphics, tk);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please Select Array Size", "Select Array Size", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
